463bf973b735a6476beadf06635798329330609c
JoelMathewPhilip3 <96024427+JoelMathewPhilip3@users.noreply.github.com>
2026-07-18 12:32:03 -0400

    Update LauncherSettings.kt

--- app/src/main/java/com/joel/minimallauncher/model/LauncherSettings.kt
+++ app/src/main/java/com/joel/minimallauncher/model/LauncherSettings.kt
@@ -8,6 +8,7 @@ data class LauncherSettings(
     val reduceGestures: Boolean = false,
     val hapticFeedback: Boolean = true,
     val doubleTapLock: Boolean = false,
+    val launcherIdleLockSeconds: Int = 0,
     val showMorningReading: Boolean = true,
     val onboardingComplete: Boolean = false
 )
